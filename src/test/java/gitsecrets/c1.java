9a23c98f2a23909b612700f2d14fd73210a50615
krishna <krishna@192.168.0.7>
2022-03-05 16:01:31 +0530

    yyyy

--- src/test/java/gitsecrets/c1.java
+++ src/test/java/gitsecrets/c1.java
@@ -20,7 +20,7 @@ public class c1 {
 		System.out.println(new TestData().properties.getProperty("t3"));	
 		
 		
-		RestAssured.baseURI="https://www.boredapi.com/";
+		RestAssured.baseURI="https://www."+new TestData().properties.getProperty("ttestpi")+".com/";
 		RestAssured.basePath="api/activity";
 		
 		RestAssured.given().log().all().get().prettyPrint();
