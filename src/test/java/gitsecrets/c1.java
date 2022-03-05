967f7fdb75f8c2a3baa9b983a3d4136e386fbe2d
krishna <krishna@192.168.0.7>
2022-03-05 16:28:34 +0530

    br1

--- src/test/java/gitsecrets/c1.java
+++ src/test/java/gitsecrets/c1.java
@@ -26,6 +26,24 @@ public class c1 {
 		RestAssured.given().log().all().get().prettyPrint();
 		
 		
+		RestAssured.baseURI="https://www."+new TestData().properties.getProperty("ttestapi")+".com/";
+		RestAssured.basePath="api/activity";
+		
+		RestAssured.given().log().all().get().prettyPrint();
+		
+		
+		RestAssured.baseURI="https://www."+new TestData().properties.getProperty("ttestapi")+".com/";
+		RestAssured.basePath="api/activity";
+		
+		RestAssured.given().log().all().get().prettyPrint();
+		
+		
+		RestAssured.baseURI="https://www."+new TestData().properties.getProperty("ttestapi")+".com/";
+		RestAssured.basePath="api/activity";
+		
+		RestAssured.given().log().all().get().prettyPrint();
+		
+		
 		
 		
 	}
